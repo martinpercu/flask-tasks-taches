1f491342666cf6c23b63212f5fcaf27c459c21e6
Martin Mendez <martinpercu@gmail.com>
2022-06-10 17:04:58 +0100

    Sign in 1st part Firestore

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -7,8 +7,13 @@ firebase_admin.initialize_app(credential)
 
 db = firestore.client()
 
+
 def get_users():
     return db.collection('users').get()
+
+
+def get_user(user_id):
+    return db.collection('users').document(user_id).get()
 
 
 def get_todos(user_id):
